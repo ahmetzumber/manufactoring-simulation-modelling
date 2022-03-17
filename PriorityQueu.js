7c100c3d6102371a77a8b34dac3f38555b4a7b1d
ahmetzumber <ahmetzumber5@gmail.com>
2022-03-17 12:40:56 +0300

    priority queu ds created

---
+++ PriorityQueu.js
@@ -0,0 +1,35 @@
+class Node {
+    constructor(element, priority) {
+        this.element = element;
+        this.priority = priority;
+    }
+}
+
+export default class PriorityQueue {
+    constructor() {
+        this.items = [];
+    }
+    enqueu(item,priority){
+        let node = new Node(item, priority);
+        let contain = false;
+
+        for (let i = 0; i < this.items.length; i++) {
+            if (this.items[i].priority > node.priority) {
+                this.items.splice(i, 0, node);
+                contain = true;
+                break;
+            }
+        }
+        if (!contain) {
+            this.items.push(node);
+        }
+    }
+    dequeue() {
+        if (this.items.length === 0)
+            return "Underflow";
+        return this.items.shift();
+    }
+
+}
+
+
